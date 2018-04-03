8c40278de2ce28ae503560f261a26f8e5d6846f0
Roman Sitchuk <yaalex438@gmail.com>
2018-04-03 05:47:55 +0300

    Tasks 3112, 3113 are complete

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task31.task3112;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/*
+Загрузчик файлов
+*/
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt", Paths.get("E:/MyDownloads"));
+
+        for (String line : Files.readAllLines(passwords)) {
+            System.out.println(line);
+        }
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        URL url = new URL(urlString);
+        InputStream stream = url.openStream();
+        Path tempFile = Files.createTempFile("smp12-", ".file");
+        Files.copy(stream, tempFile);
+        Path ultimateFile = Paths.get(downloadDirectory.toString(), urlString.substring(urlString.lastIndexOf("/")));
+        Files.move(tempFile, ultimateFile);
+        return ultimateFile;// implement this method
+    }
+}
